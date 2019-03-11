66cbe7cdf57d3dcae34e1c4296eb962e709ba964
Maria Wieckowska <mkw98@gazeta.pl>
2019-03-11 23:50:00 +0100

    add navbar

--- client/modules/App/components/Navbar/Navbar.js
+++ client/modules/App/components/Navbar/Navbar.js
@@ -3,6 +3,21 @@ import { Link } from 'react-router';
 
 import styles from './Navbar.css';
 
-
-
+export function Navbar() {
+  return (
+    <div className={styles.navbar}>
+        <ul>
+        	<li><Link to="/home">
+            	<p className={styles.navbar_text}>Home</p>
+        	</Link></li>
+        	<li><Link to="/">
+            	<p className={styles.navbar_text}>Posts</p>
+        	</Link></li>
+        	<Link to="/about">
+            	<p className={styles.navbar_text}>About</p>
+        	</Link>
+        </ul>
+    </div>
+  );
+}
 export default Navbar;
